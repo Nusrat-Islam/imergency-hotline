3aed9284af8602a4288057c267f46d1ca560c89a
Nusrat Islam Bonna <nishinusrat395@gmail.com>
2025-08-28 10:14:15 +0600

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
 
 
-document.getElementById('heart').addEventListener('click',function(){
+ const heartBtns = document.getElementsByClassName("heart");
+ 
+ for(const heartBtn of heartBtns){
+    heartBtn.addEventListener('click', function(){
+       const heartNumber = document.getElementById("number")
+          let heartNumberCount = parseInt(heartNumber.innerText)
+    heartNumberCount++;
+
+    heartNumber.innerText = heartNumberCount;
+    })
+ }
+
+
+
     
-})
